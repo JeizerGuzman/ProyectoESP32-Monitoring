227a947e76b94b346c80461b8f00629c5bf239ca
Usuario <usuario@email.com>
2026-04-29 00:56:56 -0600

    Añadio URL SQL

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ db = SQLAlchemy()
 
 class Config:
     # ✅ Variables de entorno para producción (Railway, etc)
-    DATABASE_URL = os.getenv('DATABASE_URL', 'mysql+pymysql://root:davidlaid@localhost:3306/monitoreo')
+    DATABASE_URL = os.getenv('DATABASE_URL', 'mysql://root:fKJEAFpNdvkaswbcIVfpcDSTHOwwQjQT@mysql.railway.internal:3306/TrackSecurity')
     SQLALCHEMY_DATABASE_URI = DATABASE_URL
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = os.getenv('SECRET_KEY', 'innovatecsecurity2026')
